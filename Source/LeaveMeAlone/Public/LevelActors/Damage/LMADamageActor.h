339e7ef55a85f3b45a6f1bfeb129d3e9a42aa838
goleg5281 <goleg5281@gmail.com>
2025-03-12 11:21:02 +0500

    state machine almost done sprint

---
+++ Source/LeaveMeAlone/Public/LevelActors/Damage/LMADamageActor.h
@@ -0,0 +1,39 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "LMADamageActor.generated.h"
+
+UCLASS()
+class LEAVEMEALONE_API ALMADamageActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALMADamageActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Dagmage")
+	float Damage = 5.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Dagmage")
+	float SphereRadius = 100.0f;
+};
