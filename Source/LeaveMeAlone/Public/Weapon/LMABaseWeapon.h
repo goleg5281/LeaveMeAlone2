9970fed5e540bfffb3f4c6b98cb715cdb6de3cef
goleg5281 <goleg5281@gmail.com>
2025-03-13 17:40:46 +0500

    Migrate Weapon

---
+++ Source/LeaveMeAlone/Public/Weapon/LMABaseWeapon.h
@@ -0,0 +1,31 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LMABaseWeapon.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class LEAVEMEALONE_API ALMABaseWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALMABaseWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Weapon")
+	USkeletalMeshComponent* WeaponComponent;
+};
